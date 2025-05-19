95079853435896f32fd45235c32454d663453c9c
lxthxrgx <osu.bonse@gmail.com>
2025-05-19 18:09:19 +0300

    initial

---
+++ src/app/components/NavMenu.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Link from 'next/link';
+
+export default function NavMenu() {
+  return (
+    <nav className="w-full flex justify-center gap-8 py-4 bg-white/10 backdrop-blur-md border-b border-white/20 text-sm font-medium">
+        <Link href="/" className="hover:underline">Головна</Link>
+        <Link href="/contact" className="hover:underline">Контрагенти</Link>
+        <Link href="/group" className="hover:underline">Групи</Link>
+        <Link href="/contact" className="hover:underline">Суборенда</Link>
+        <Link href="/contact" className="hover:underline">Охорона</Link>
+    </nav>
+  );
+}
