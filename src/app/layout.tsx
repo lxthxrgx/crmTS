2fb47d82295d8e586c865524fc7a110cfd98a3bc
lxthxrgx <osu.bonse@gmail.com>
2025-06-18 21:50:24 +0300

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import NavMenu from "./components/NavMenu";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,9 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}>
+        <NavMenu />
         {children}
       </body>
     </html>
