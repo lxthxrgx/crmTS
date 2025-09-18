99ea0ddfda0d75edbc85b4f73e21f222048a635d
lxthxrgx <osu.bonse@gmail.com>
2025-09-18 20:11:27 +0300

    some changes

--- src/app/agreements-now/page.tsx
+++ src/app/agreements-now/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/navigation'
 import "../css/card.css"
 
 export default function Aggrements() {
-    const router = useRouter()
 
     const DataSublease:CardData[] = [
         {
@@ -107,12 +106,6 @@ export default function Aggrements() {
     )
     return (
          <div className="p-4">
-            <button
-                onClick={() => router.push('/')}
-                className="bg-white text-black text-lg px-4 py-2 rounded-2xl shadow-lg m-5 hover:bg-gray-200 transition"
-            >
-                ← Назад
-            </button>
 
             <div className="card-title">
                 <h1>Суборенда</h1>
