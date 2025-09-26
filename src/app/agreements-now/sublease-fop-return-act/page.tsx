163936b4f064c81115d3e7dbdf7ee4a61e700534
lxthxrgx <osu.bonse@gmail.com>
2025-09-26 11:33:51 +0300

    update

--- src/app/agreements-now/sublease-fop-return-act/page.tsx
+++ src/app/agreements-now/sublease-fop-return-act/page.tsx
@@ -3,6 +3,8 @@ import ISubleaseFopReturnAct from '@/app/model/IsubleaseFopReturnAct';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { AreaToText } from '@/app/components/numberToText';
+import FloatInput from '@/app/components/FloatInput';
+import "../../css/card.css"
 
 export default function SubleaseFopReturnAct() {
     const router = useRouter();
@@ -83,62 +85,63 @@ export default function SubleaseFopReturnAct() {
         <div className="text-white min-h-screen p-6">
         <button
             className="bg-white text-black text-lg px-4 py-2 rounded-2xl shadow-lg mb-6 hover:bg-gray-200 transition"
-            onClick={() => router.push('/agreements-now')}
-        >← Назад</button>
+            onClick={() => router.push('/agreements-now')} >← Назад</button>
 
         <div className="max-w-4xl mx-auto">
             <h1 className="text-2xl font-bold mb-6 text-center">Форма акту для повернення приміщення (ТОВ)</h1>
             <form onSubmit={handleSubmit} className="space-y-4">
 
-            <div className='backdrop-blur-md bg-white/20 border border-white/30 rounded-xl shadow-lg p-6'>
+            <div className=' card-container'>
+
+                <FloatInput id='NumberGroup' label='Площа' value={data.RoomArea} onChange={handleChange}/>
                 <label className="block">Номер відділення</label>
-                <input type="text" name="NumberGroup" value={data.NumberGroup} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="NumberGroup" value={data.NumberGroup} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">Найменування відділення</label>
-                <input type="text" name="NameGroup" value={data.NameGroup} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }} />
+                <input type="text" name="NameGroup" value={data.NameGroup} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }} />
                 <label className="block">Дата створення угоди</label>
-                <input type="date" name="CreationDate" value={data.CreationDate.toISOString().substring(0,10)} onChange={handleDateChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="date" name="CreationDate" value={data.CreationDate.toISOString().substring(0,10)} onChange={handleDateChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
             </div>
 
-            <div className='backdrop-blur-md bg-white/20 border border-white/30 rounded-xl shadow-lg p-6'>
+            <div className=' card-container'>
                 <h1 className='text-center'>Договір</h1>
               <label className="block">Номер</label>
-                <input type="text" name="ContractNumber" value={data.ContractNumber} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }} />
+                <input type="text" name="ContractNumber" value={data.ContractNumber} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }} />
                 <label className="block">Дата укладання</label>
-                <input type="date" name="CreationContractDate" value={data.CreationContractDate.toISOString().substring(0,10)} onChange={handleDateChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="date" name="CreationContractDate" value={data.CreationContractDate.toISOString().substring(0,10)} onChange={handleDateChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
             </div>
 
-            <div className='backdrop-blur-md bg-white/20 border border-white/30 rounded-xl shadow-lg p-6'>
+            <div className=' card-container'>
                 <h1 className='text-center'>Суборендодавець</h1>
                 <label className="block">ПІП</label>
-                <input type="text" name="PipSublessor" value={data.PipSublessor} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="PipSublessor" value={data.PipSublessor} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">ПІП (скорочено)</label>
-                <input type="text" name="PipsSublessor" value={data.PipsSublessor} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="PipsSublessor" value={data.PipsSublessor} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">РНОКПП</label>
-                <input type="text" name="rnokppSublessor" value={data.rnokppSublessor} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="rnokppSublessor" value={data.rnokppSublessor} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">ЄДРЮОФОП</label>
-                <input type="text" name="Edruofop" value={data.Edruofop} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="Edruofop" value={data.Edruofop} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">Адреса</label>
-                <input type="text" name="addressSublessor" value={data.addressSublessor} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="addressSublessor" value={data.addressSublessor} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">Рахунок та назва банку</label>
-                <input type="text" name="BanckAccount" value={data.BanckAccount} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="BanckAccount" value={data.BanckAccount} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
             </div>
             
-            <div className='backdrop-blur-md bg-white/20 border border-white/30 rounded-xl shadow-lg p-6'>
+            <div className=' card-container'>
                 <h1 className='text-center'>Приміщення</h1>
                 <label className="block">Площа</label>
-                <input type="number" name="RoomArea" value={data.RoomArea} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="number" name="RoomArea" value={data.RoomArea} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">Площа (текстом)</label>
-                <input type="text" name="RoomAreaText" value={data.RoomAreaText} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="RoomAreaText" value={data.RoomAreaText} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
 
                 <label className="block">Місцезнаходження</label>
-                <input type="text" name="RoomAreaAddress" value={data.RoomAreaAddress} onChange={handleChange} className="w-full p-3 bg-f bg-opacity-50 text-black placeholder-white rounded" style={{ backgroundColor: "#ffffffa8" }}/>
+                <input type="text" name="RoomAreaAddress" value={data.RoomAreaAddress} onChange={handleChange} className=" " style={{ backgroundColor: "#ffffffa8" }}/>
             </div>
 
             <button type="submit" className="w-full hover:bg-blue-700 text-black py-3 rounded-lg" style={{ backgroundColor: "#ffffffa8" }}>
